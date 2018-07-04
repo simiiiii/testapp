a4d1d30e8cb31fb838d28abce50b7617e7448596
Simone <simvdpas@hotmail.com>
2018-07-04 12:27:34 +0200

    kijken of het nu wel werkt

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/about", function(req, res){
-    res.render("about");
-});
+
 
 app.listen(process.env.PORT, process.env.IP);
